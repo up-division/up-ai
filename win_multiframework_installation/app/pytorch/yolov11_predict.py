b9ecfae42a6e7f9d664492080d5ae11f3e93f35a
DESKTOP-JRQ67FA\Jasmine <Jasmine1Chen@aaeon.com.tw>
2025-01-20 13:45:18 +0800

    linux update:VideoPlayer helptext display

--- win_multiframework_installation/app/pytorch/yolov11_predict.py
+++ win_multiframework_installation/app/pytorch/yolov11_predict.py
@@ -2,14 +2,16 @@ from ultralytics import YOLO
 import argparse
 import cv2
 import os
-#import mo
+import time
+import collections
+import numpy as np
 from monitor import monitor
 
 model = YOLO('yolo11n.pt')  # 使用預訓練的 YOLOv8 模型
 
 def arg_parser() :
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_source", help="Please input inference source.ex. C:\\<path>\\inference_video.mp4 or camera number")
+    parser.add_argument("input_source", help="Please input inference source.ex. C:\\<path>\\inference_vedio.mp4 or camera number")
     return parser
 # load YOLO model（"yolov8n", "yolov8s" ...）
 
@@ -17,37 +19,72 @@ def main(args):
     if  args.input_source.isdigit():
         print("Init USB Camera in Windows,Please wait about 20 Second!")
         cap = cv2.VideoCapture(0)
+        if not cap.isOpened():
+            print("Error: Unable to access the camera.")
+            sys.exit()
     else:
         print("Streaming video!")
-        # now_dir=os.path.dirname(os.path.abspath(__file__))
-        # now_dir=now_dir.split('\\')
-        # video_path=now_dir[:-2]
-        # video_path=os.path.join('\\'.join(video_path),'videos','people.mp4')# 影片路徑
-        # print(video_path)
-        # cap = cv2.VideoCapture(video_path)
         cap = cv2.VideoCapture(args.input_source)
-    vedio_h,vedio_w = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+    # vedio_h,vedio_w = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    video_h,video_w = (720,1280)
+
     from monitor import monitor
-    monitor=monitor(vedio_h,vedio_w)
+    monitor=monitor(video_h,video_w)
     monitor.start_cpu_monitor()
     monitor.start_mem_monitor()
+
+    processing_times = collections.deque()
+
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
-            
+        else:
+            frame_resized = cv2.resize(frame, (video_w,video_h))
+            # print(frame_resized.shape)
+
+        start_time = time.time()
+
         # 進行推論
-        results = model(frame)
+        results = model(frame_resized)
+
         annotated_frame = results[0].plot()
+        monitor.draw_helptext(annotated_frame)
         if monitor.show_device['CPU']:
             chart = monitor.draw_cpu_chart()
-            annotated_frame[-(vedio_h//3):, 10:-10] = chart  # put cpu monitor in bottom
+            annotated_frame[-(video_h//3):, 10:-10] = chart  # put cpu monitor in bottom
         if monitor.show_device['Memory']:
             annotated_frame=monitor.draw_memory_usage_bar(annotated_frame)
+
+        stop_time = time.time()
+        processing_times.append(stop_time - start_time)
+
+        if len(processing_times) > 200:
+            processing_times.popleft()
+
+        processing_time = np.mean(processing_times) * 1000  # ms
+        fps = 1000 / processing_time
+
+        frame_height, frame_width = annotated_frame.shape[:2]
+        annotated_frame = cv2.putText(
+            annotated_frame,
+            f"Inference time: {processing_time:.1f}ms ({fps:.1f} FPS)",
+            org=(20, 110),
+            fontFace=cv2.FONT_HERSHEY_COMPLEX,
+            fontScale=frame_width / 1500,
+            color=(0, 0, 255),
+            thickness=2,
+            lineType=cv2.LINE_AA,
+        )
+
+
         # show result
         cv2.imshow('YOLO Video Prediction', annotated_frame)
         input_key=cv2.waitKey(1)
-        if input_key & 0xFF == ord('q'):
+        if input_key == 27:
+            break
+        elif input_key & 0xFF == ord('q'):
             monitor.stop_cpu_monitor()
             monitor.stop_mem_monitor()
             break
@@ -78,7 +115,6 @@ def main(args):
     monitor.stop_mem_monitor()
     cap.release()
     cv2.destroyAllWindows()
-
 if __name__ == "__main__":
     args = arg_parser().parse_args()
     main(args)
