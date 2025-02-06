bd48e21f461002c3eaae1596be3804ded04e8355
DannyZhang <DannyZhang@aaeon.com.tw>
2025-02-06 15:01:33 +0800

    [windows]download chatbot model

--- win_multiframework_installation/env_list/py310/ov-chatbot.bat
+++ win_multiframework_installation/env_list/py310/ov-chatbot.bat
@@ -9,3 +9,21 @@ pip install -r %currentDir%\..\chatbot\requirements.txt
 echo ===========================================
 echo Chatbot Environment Installation Completed!
 echo ===========================================
+echo "Check Chatbot Model!"
+set FILE_URL=https://aaeon365-my.sharepoint.com/:u:/g/personal/dannyzhang_aaeon_com_tw/Ee8WFbG9QvRPmIUYY859Gr4B3CXQhMlG2_m0ROpBYVXfsg?download=1
+set "model_path=%currentDir%\..\chatbot\tiny-llama-1b-chat\INT4_compressed_weights\openvino_model.bin"
+@REM get model size(local)
+for %%A in ("%model_path%") do set FILE_SIZE=%%~zA
+@REM SIZE_LIMIT=10MB
+set /a SIZE_LIMIT=10485760
+echo Chatbot Model Size:%FILE_SIZE%
+if %FILE_SIZE% LSS %SIZE_LIMIT% (
+    del %model_path%
+)
+if exist %model_path% (
+    echo Chatbot model exist.
+) else (
+    echo Download Chatbot Model now!
+    powershell -command "wget %FILE_URL% -o %model_path%"
+    echo Download done!
+)
