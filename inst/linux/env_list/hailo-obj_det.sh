03fc3d81404e81c4972326b146570497d3652e6c
123 <louischen@aaeon.com.tw>
2025-12-17 16:27:34 +0800

    add Hailo-8L support

--- inst/linux/env_list/hailo-obj_det.sh
+++ inst/linux/env_list/hailo-obj_det.sh
@@ -1,44 +1,38 @@
 #!/bin/bash
 
-tappas_version=$(grep -A1 "^project" "$HOME/tappas/core/hailo/meson.build" | grep "version" | sed -E "s/.*version *: *'([0-9.]+)'.*/\1/")
-
-if [ "$tappas_version" == "3.30.0" ]; then
-    sudo rm -rf "$HOME/tappas"
-fi
-
 echo ====================================================
 echo $'\t'Install Hailo Object Detect Packages
 echo ====================================================
 
-if [ -f "$HOME/tappas/scripts/bash_completion.d/python-argcomplete" ]; then
+if [ -d "$PWD/inst/linux/app/hailo-apps-infra/venv_hailo_apps" ]; then
     echo ======================================================
     echo $'\t'Hailo Object Detect  is already exist!
     echo ======================================================
     read -p "Do you want to delete and reinstall?(y/n): " answer
     if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
-        rm -f "$HOME/tappas/scripts/bash_completion.d/python-argcomplete"
+        sudo rm -rf "$PWD/inst/linux/app/hailo-apps-infra/venv_hailo_apps"
     else
         exit 0
     fi
 fi
 
-echo "Install Hailo tappas packages......"
-#install require package
-sudo apt-get install -y rsync ffmpeg x11-utils python3-dev python3-pip python3-setuptools python3-virtualenv python-gi-dev libgirepository1.0-dev gcc-12 g++-12 cmake git libzmq3-dev pkg-config
-#install cv
-sudo apt-get install -y libopencv-dev python3-opencv
-#install gstreamer
-sudo apt-get install -y libcairo2-dev libgirepository1.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio gcc-12 g++-12 python-gi-dev
-#install pygobject
-sudo apt install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0
-#install tappas
-git clone -b v5.1.0 --depth=1 https://github.com/hailo-ai/tappas.git $HOME/tappas
-cd $HOME/tappas
-./install.sh --skip-hailort
+ubuntu_version=$(lsb_release -rs)
 
-if [ -f "$HOME/tappas/scripts/bash_completion.d/_python-argcomplete" ];then
-    mv $HOME/tappas/scripts/bash_completion.d/_python-argcomplete $HOME/tappas/scripts/bash_completion.d/python-argcomplete
-fi
-
-echo "Hailo Object detect Environment Installation Completed!"
-
+case "$ubuntu_version" in
+        24.04)
+            git clone -b v25.10.0 --depth=1 https://github.com/hailo-ai/hailo-apps-infra.git $PWD/inst/linux/app/hailo-apps-infra
+            cd $PWD/inst/linux/app/hailo-apps-infra
+            yes | sudo ./scripts/hailo_installer.sh --hw-arch=hailo8
+            sudo ./install.sh
+            ;;
+        22.04)
+            git clone -b 25.7.0 --depth=1 https://github.com/hailo-ai/hailo-apps-infra.git $PWD/inst/linux/app/hailo-apps-infra
+            cd $PWD/inst/linux/app/hailo-apps-infra
+            yes | sudo ./scripts/hailo_installer.sh
+            sudo ./install.sh
+            ;;
+        *)
+            echo "Unsupported Ubuntu version: $ubuntu_version. Exiting."
+            exit 1
+            ;;
+esac
